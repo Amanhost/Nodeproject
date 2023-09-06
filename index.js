2bd422fe79d6a6a325b7788e6d133901d235c3a0
Aman prakash <aman.prakash@synoriq.in>
2023-09-07 00:05:59 +0530

    node first changes

---
+++ index.js
@@ -0,0 +1,23 @@
+const lib = require('./lib.js'); //tradisonal way
+const express = require('express');
+console.log('Hellow ');
+const server = express();
+server.listen(8080);
+
+// import { sum, sub } from './lib.js';
+//reade the file Node is a server we can not stopt the server
+const fs = require('fs');
+// const txt = fs.readFileSync('demo.txt', 'utf-8');
+// console.log(performance.now());
+// fs.readFile('demo.txt', 'utf-8', (error, text) => {
+//   console.log(text);
+// });
+// // console.log(txt);
+// console.log(lib.sum(4, 5));
+// console.log(performance.now());
+// console.log(lib.sub(3, 6));
+
+/* but now ES module */
+
+/*
+ */
---
+++ lib.js
@@ -0,0 +1,8 @@
+exports.sum = (a, b) => {
+  return a + b;
+};
+
+exports.sub = (a, b) => {
+  return a - b;
+};
+//this is object short hand
